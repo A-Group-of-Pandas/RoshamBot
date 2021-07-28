c24cf5d5688362d914aff6aaf35fa5b2746acdf9
akshatat777 <85193006+akshatat777@users.noreply.github.com>
2021-07-28 14:04:28 -0700

    Added Interface.py

---
+++ Interface.py
@@ -0,0 +1,30 @@
+import pygame
+
+pygame.init()
+
+
+display_width = 800
+display_height = 600
+
+gameDisplay = pygame.display.set_mode((display_width,display_height))
+pygame.display.set_caption('ROSHAMBOT!')
+
+black = (0,0,0)
+white = (255,255,255)
+
+clock = pygame.time.Clock()
+crashed = False
+
+while not crashed:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            crashed = True
+
+    gameDisplay.fill(white)
+    car(x,y)
+
+    pygame.display.update()
+    clock.tick(60)
+
+pygame.quit()
+quit()
